b1f40f9f823a6c9e88d10b29e060a04047cde280
Michel <mijstonen@hotmail.com>
2018-02-12 05:29:22 +0100

    Incorporate C++17 filesystem.

--- inc/cpx-common-impl.cpp
+++ inc/cpx-common-impl.cpp
@@ -4,11 +4,14 @@
 #  define CPX_COMMON_IMPL_CPP 1
 
 /* This file can be included to be compiled with the cpx script source code
+ * It is as if this is in front of your scripting code at file scope.
  *
  * This file is included in cpx-main-definition-signature.hpp
  * ( Usage: #+common-impl.cpp )
  */
 
+
+namespace fs = std::experimental::filesystem;
 
 namespace cpx {
 // #include ...
